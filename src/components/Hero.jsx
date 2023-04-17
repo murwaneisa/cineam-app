38a1f3f40e559463e8c082ea7766ba96687e5445
Murwan Eisa <eisa.murwan@gmail.com>
2023-04-17 05:14:22 +0200

    styling the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,32 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Stack, Badge } from "react-bootstrap";
+import "../../hero.css";
 
 function HeroSection() {
   return (
-    <Container
-      style={{
-        backgroundImage: `url(https://images.thedirect.com/media/article_full/avatar-2-reviews.jpg)`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        minHeight: "80vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <Container>
-        <h1>The Lord of the Rings: The Fellowship of the Ring</h1>
-        <p className="lead">
-          Directed by Peter Jackson | Starring Elijah Wood, Ian McKellen,
-          Orlando Bloom
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-          ullamcorper mauris, vel molestie quam commodo ac. Praesent suscipit
-          nulla non neque faucibus, sit amet bibendum ipsum commodo. In
-          eleifend, erat non faucibus consequat, neque nunc vestibulum felis, a
-          aliquet sapien mi quis purus.
-        </p>
-        <p>
-          <Button variant="primary" size="lg" className="mr-2">
-            Book Tickets
-          </Button>
-          <Button variant="outline-light" size="lg">
-            Watch Trailer
-          </Button>
-        </p>
-      </Container>
-    </Container>
+    <header className="masthead">
+      <div className="container h-100 text-left">
+        <div className="row h-100 align-items-end ">
+          <div className="col-12 mb-4">
+            <h1 className="fw-light">Black Panther</h1>
+            <span className="date">2h 14m | 2018</span>
+            <p className="lead w-50 ">
+              T'Challa, heir to the hidden but advanced kingdom of Wakanda, must
+              step forward to lead his people into a new future and must
+              confront a challenger from his country's past.
+            </p>
+            <p>
+              Director: <span class="director">Ryan Coogler</span>
+            </p>
+
+            <Stack direction="horizontal" gap={3}>
+              <Button variant="primary">Get Ticket</Button>
+              <Button variant="secondary">Watch Trailer</Button>
+            </Stack>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
