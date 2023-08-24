99e10a4e39d201dbe18f3b877549d1e01fe96306
Murwan Eisa <eisa.murwan@gmail.com>
2023-08-24 14:32:01 +0200

    finished the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Col, Container, Image, Row, Stack } from "react-bootstrap";
+import { Button, Col, Container, Image, Row, Stack } from "react-bootstrap";
 import { IoIosCall } from "react-icons/io";
 import { FaFacebook } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { MdEmail, MdLocationPin } from "react-icons/md";
 import "../style/footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -13,52 +14,74 @@ function Footer() {
         <Row>
           <Col
             md={4}
-            className="d-flex align-items-start justify-content-between mb-4"
+            className="d-flex align-items-start justify-content-between mb-4 sec-container"
           >
             <div>
-              <Image src={"/images/logo2.png"} className="footer-logo" />
+              <a href="#" style={{ textDecoration: "none" }}>
+                <Image src={"/images/logo2.png"} className="footer-logo" />
+              </a>
               <p>Follow Us On Social Media</p>
               <div className="d-flex flex-row  gap-3 align-items-center">
                 <div className="icon-btn">
-                  <FaFacebook style={{ color: "#ffffff", fontSize: "24px" }} />
+                  <a href="#" style={{ textDecoration: "none" }}>
+                    <FaFacebook
+                      style={{ color: "#ffffff", fontSize: "24px" }}
+                    />
+                  </a>
                 </div>
                 <div className="icon-btn">
-                  <AiFillInstagram
-                    style={{ color: "#ffffff", fontSize: "24px" }}
-                  />
+                  <a href="#" style={{ textDecoration: "none" }}>
+                    <AiFillInstagram
+                      style={{ color: "#ffffff", fontSize: "24px" }}
+                    />
+                  </a>
                 </div>
                 <div className="icon-btn">
-                  <IoIosCall style={{ color: "#ffffff", fontSize: "24px" }} />
+                  <a href="#" style={{ textDecoration: "none" }}>
+                    <IoIosCall style={{ color: "#ffffff", fontSize: "24px" }} />
+                  </a>
                 </div>
               </div>
             </div>
           </Col>
-          <Col md={4} className="mb-4">
+          <Col md={4} className="mb-4 sec-container">
             <h3>Meet At Start Cinema</h3>
-            <p>About us </p>
-            <p>Privacy Policy </p>
-            <p>Student Reward Program</p>
-            <p>Terms & Condition</p>
+            <a href="#" style={{ textDecoration: "none" }}>
+              <p>About us </p>
+            </a>
+            <a href="#" style={{ textDecoration: "none" }}>
+              <p>Privacy Policy </p>
+            </a>
+            <a href="#" style={{ textDecoration: "none" }}>
+              <p>Student Reward Program</p>
+            </a>
+            <a href="#" style={{ textDecoration: "none" }}>
+              <p>Terms & Condition</p>
+            </a>
           </Col>
-          <Col md={6}>
+          <Col md={4} className="sec-container">
             <h3>Contact Us</h3>
             <div className="d-flex flex-row mb-3  align-items-center">
               <div className="icon-btn">
                 <IoIosCall style={{ color: "#ffffff", fontSize: "24px" }} />
               </div>
-              <p className="m-3">+73 567 45676</p>
+              <p className="mx-3">073 567 45676</p>
             </div>
-            <div className="d-flex flex-row mb-3 align-items-center">
+            <div className="d-flex flex-row mb-3  align-items-center">
               <div className="icon-btn">
                 <MdEmail style={{ color: "#ffffff", fontSize: "24px" }} />
               </div>
-              <p className="m-3">customer@st.cinema. com</p>
+              <p className="m-3" style={{ wordBreak: "break-all" }}>
+                customer@st.cinema.com
+              </p>
             </div>
             <div className="d-flex flex-row mb-3  align-items-center">
               <div className="icon-btn">
                 <MdLocationPin style={{ color: "#ffffff", fontSize: "24px" }} />
               </div>
-              <p className="m-3">Östrakasering 5, Kristianstad</p>
+              <p className="m-3" style={{ wordBreak: "break-all" }}>
+                Östrakasering5, Kristianstad
+              </p>
             </div>
           </Col>
         </Row>
